92b05e758f677710d8937dc59d96f3d6c25dc205
gnat1923 <ckenny94@gmail.com>
2023-07-30 18:41:08 +0200

    second commit

--- hello.py
+++ hello.py
@@ -18,3 +18,15 @@ def index():
 
 def user(name):
     return render_template("user.html", user_name=name)
+
+# Custom error pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
