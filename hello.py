9690f1e02a778237dd17665b8af1e314c5fbf128
gnat1923 <ckenny94@gmail.com>
2023-08-06 18:54:23 +0200

    Added database functionality

--- hello.py
+++ hello.py
@@ -2,10 +2,35 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create flask instance
 app = Flask(__name__)
+# Add database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+# Secret key
 app.config["SECRET_KEY"] = "this is the secret key"
+# Initialise the database
+db = SQLAlchemy(app)
+app.app_context().push()
+
+# Create model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create a string
+    def __repr__(self):
+        return "<Name %r>" % self.name
+    
+# Create a user form class
+class UserForm(FlaskForm):
+    name = StringField("Name:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a form class
 class NamerForm(FlaskForm):
@@ -54,5 +79,27 @@ def name():
         name = name,
         form = form                   
         )
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User added successfully")
+
+    our_users = Users.query.order_by(Users.date_added)
+
+    return render_template("add_user.html",
+                            form=form,
+                            name=name,
+                            our_users=our_users)
 
 
